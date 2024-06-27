49b071ebe5233998b3cfabd1ab3f9abcf146567f
3472592 <3472592@gmail.com>
2024-06-27 06:00:21 -0400

    Add files via upload

---
+++ Excuse.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.Serialization;
+
+namespace ExcuseManager
+{
+
+    [DataContract(Namespace = "http://www.headfirstlabs.com/ExcuseManager")]
+    class Excuse : INotifyPropertyChanged
+    {
+        [DataMember]
+        public string Description { get; set; }
+
+        [DataMember]
+        public string Results { get; set; }
+
+        [DataMember]
+        public DateTimeOffset LastUsedDate { get; set; }
+
+        public Excuse()
+        {
+            LastUsedDate = DateTimeOffset.Now;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler propertyChangedEvent = PropertyChanged;
+            if (propertyChangedEvent != null)
+            {
+                propertyChangedEvent(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace ExcuseManager
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private readonly ExcuseManager excuseManager;
+        public MainPage()
+        {
+            this.InitializeComponent();
+            DataContext = excuseManager = new ExcuseManager();
+        }
+
+        private void OpenButtonClick(object sender, RoutedEventArgs e)
+        {
+            excuseManager.OpenExcuseAsync();
+        }
+
+        private void SaveButtonClick(object sender, RoutedEventArgs e)
+        {
+            excuseManager.SaveCurrentExcuseAsync();
+        }
+
+        private void NewExcuseButtonClick(object sender, RoutedEventArgs e)
+        {
+            excuseManager.NewExcuseAsync();
+        }
+
+        private void SaveAsButtonClick(object sender, RoutedEventArgs e)
+        {
+            excuseManager.SaveCurrentExcuseAsAsync();
+        }
+
+        private void RandomExcuseButtonClick(object sender, RoutedEventArgs e)
+        {
+            excuseManager.OpenRandomExcuseAsync();
+        }
+
+        private async void FolderButtonClick(object sender, RoutedEventArgs e)
+        {
+            bool folderChosen = await excuseManager.ChooseNewFolderAsync();
+            if (folderChosen)
+            {
+                saveButton.IsEnabled = true;
+                randomButton.IsEnabled = true;
+            }
+        }
+
+        private void SetToCurrentTimeClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
